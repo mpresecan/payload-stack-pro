54ab50325d629b382938b69ac9e7f941d1782933
mpresecan <m_presecan@yahoo.com>
2024-10-04 14:29:05 +0200

    advent ecosystem added link

--- src/app/(frontend)/(public)/_components/sections/logos.tsx
+++ src/app/(frontend)/(public)/_components/sections/logos.tsx
@@ -1,14 +1,33 @@
-import Marquee from "@/components/magicui/marquee";
-import Image from "next/image";
+import Marquee from '@/components/magicui/marquee'
+import Image from 'next/image'
+import { Link } from 'next-view-transitions'
 
 const companies = [
-  "brand-advent-homesteads-fw",
-  "brand-advent-leaders-fw",
-  "brand-advent-truths-fw",
-  "brand-advent-life-fw",
-  "brand-advent-outposts-fw",
-  "brand-advent-outreach-fw",
-];
+  {
+    img: 'brand-advent-homesteads-fw',
+    link: 'https://adventhomesteads.com',
+  },
+  {
+    img: 'brand-advent-leaders-fw',
+    link: 'https://adventleaders.com',
+  },
+  {
+    img: 'brand-advent-truths-fw',
+    link: 'https://adventtruths.com',
+  },
+  {
+    img: 'brand-advent-life-fw',
+    link: 'https://adventlife.com',
+  },
+  {
+    img: 'brand-advent-outposts-fw',
+    link: 'https://adventoutposts.com',
+  },
+  {
+    img: 'brand-advent-outreach-fw',
+    link: 'https://adventoutreach.com',
+  },
+]
 
 export default function Logos() {
   return (
@@ -20,20 +39,24 @@ export default function Logos() {
         <div className="relative mt-6">
           <Marquee className="max-w-full [--duration:40s]">
             {companies.map((logo, idx) => (
-              <Image
-                key={idx}
-                width={112}
-                height={40}
-                src={`/${logo}.png`}
-                className="h-14 w-44 opacity-60 hover:opacity-100 transition-all duration-400 ease-in-out saturate-0 hover:saturate-100"
-                alt={logo}
-              />
+              <Link href={logo.link} key={idx} target='_blank'>
+                <Image
+                  key={idx}
+                  width={112}
+                  height={40}
+                  src={`/${logo.img}.png`}
+                  className="h-14 w-44 opacity-60 hover:opacity-100 transition-all duration-400 ease-in-out saturate-0 hover:saturate-100"
+                  alt={logo.img}
+                />
+              </Link>
             ))}
           </Marquee>
-          <div className="pointer-events-none absolute inset-y-0 left-0 h-full w-1/3 bg-gradient-to-r from-background"></div>
-          <div className="pointer-events-none absolute inset-y-0 right-0 h-full w-1/3 bg-gradient-to-l from-background"></div>
+          <div
+            className="pointer-events-none absolute inset-y-0 left-0 h-full w-1/3 bg-gradient-to-r from-background"></div>
+          <div
+            className="pointer-events-none absolute inset-y-0 right-0 h-full w-1/3 bg-gradient-to-l from-background"></div>
         </div>
       </div>
     </section>
-  );
+  )
 }
