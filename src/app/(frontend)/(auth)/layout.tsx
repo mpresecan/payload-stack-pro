893dffa83ef68936d115a0fcd1eea4b591cf1c08
mpresecan <m_presecan@yahoo.com>
2024-10-06 11:48:49 +0200

    auth layout update

--- src/app/(frontend)/(auth)/layout.tsx
+++ src/app/(frontend)/(auth)/layout.tsx
@@ -7,7 +7,7 @@ import { ChevronLeft } from 'lucide-react'
 export default function Layout({children} : React.PropsWithChildren) {
   return (
     <div className="w-full lg:grid lg:min-h-[100vh] lg:grid-cols-2">
-      <div className='flex flex-col'>
+      <div className='flex flex-col px-2 lg:px-0'>
         <Button asChild variant='outline' className='m-4 w-fit rounded-full'>
           <Link href={'/'} className='text-muted-foreground'>
             <ChevronLeft />
@@ -24,7 +24,7 @@ export default function Layout({children} : React.PropsWithChildren) {
           alt="family gazing at Jesus during His 2nd coming"
           width="1920"
           height="1080"
-          className="h-full w-full object-cover object-center dark:brightness-[0.2] pe-2"
+          className="h-full w-full object-cover object-center dark:brightness-[0.2]"
         />
       </div>
     </div>
