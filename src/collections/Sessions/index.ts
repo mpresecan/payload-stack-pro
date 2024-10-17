da5458339b397d99fc99ab6b41a21999c8ce126b
mpresecan <m_presecan@yahoo.com>
2024-10-17 07:37:27 +0300

    putting back the notification system

--- src/collections/Sessions/index.ts
+++ src/collections/Sessions/index.ts
@@ -245,7 +245,7 @@ export const Sessions: CollectionConfig = {
       async () => {
         revalidateTag('sessions')
       },
-      // notifyPotentialPresentersOnTopicSuggestionCreate,
+      notifyPotentialPresentersOnTopicSuggestionCreate,
       notifyPotentialParticipantsOnSessionCreate,
     ],
   },
